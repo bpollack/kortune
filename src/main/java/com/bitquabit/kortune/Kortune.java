4092b596042924b30f1168f0fce61ea95aa78989
Benjamin Pollack <benjamin@bitquabit.com>
2017-02-07 08:46:44 -0500

    working

--- src/main/java/com/bitquabit/kortune/Kortune.java
+++ src/main/java/com/bitquabit/kortune/Kortune.java
@@ -1,7 +1,39 @@
 package com.bitquabit.kortune;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Kortune {
-    public boolean someLibraryMethod() {
-        return true;
+    private static final Pattern divider = Pattern.compile("^%$", Pattern.MULTILINE);
+
+    private static String readAll(String resourceName) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    Kortune.class.getResourceAsStream(resourceName)));
+            char[] chars = new char[4096];
+            StringBuilder sb = new StringBuilder();
+            int read = 0;
+            while ((read = reader.read(chars)) > 0) {
+                sb.append(chars, 0, read);
+            }
+            return sb.toString();
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    private static List<String> loadFortunes(String resourceName) {
+        String contents = readAll(resourceName);
+        String[] matches = divider.split(contents);
+        return Arrays.asList(matches).subList(1, matches.length);
+    }
+
+    public static void main(String[] args) {
+        List<String> fortunes = loadFortunes("/offensive.fortunes");
+        System.out.println(fortunes.get(5));
     }
 }
