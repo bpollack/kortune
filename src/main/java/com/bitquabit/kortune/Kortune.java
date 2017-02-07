bfa1ee0a8741b1bd7e30b664a92e5f84861e8bc9
Benjamin Pollack <benjamin@bitquabit.com>
2017-02-07 10:02:52 -0500

    further cleanup, more working

--- src/main/java/com/bitquabit/kortune/Kortune.java
+++ src/main/java/com/bitquabit/kortune/Kortune.java
@@ -3,37 +3,46 @@ package com.bitquabit.kortune;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 import java.util.regex.Pattern;
 
 public class Kortune {
-    private static final Pattern divider = Pattern.compile("^%$", Pattern.MULTILINE);
-
-    private static String readAll(String resourceName) {
+    private static List<String> loadFortunes(String resourceName) {
+        final List<String> fortunes = new ArrayList<>();
         try {
-            BufferedReader reader = new BufferedReader(new InputStreamReader(
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(
                     Kortune.class.getResourceAsStream(resourceName)));
-            char[] chars = new char[4096];
-            StringBuilder sb = new StringBuilder();
-            int read = 0;
-            while ((read = reader.read(chars)) > 0) {
-                sb.append(chars, 0, read);
+            final StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.equals("%")) {
+                    fortunes.add(sb.toString());
+                    sb.setLength(0);
+                } else {
+                    sb.append(line);
+                    sb.append("\n");
+                }
             }
-            return sb.toString();
-        } catch (IOException e) {
-            return null;
+            fortunes.add(sb.toString());
+        } catch (IOException ignored) {
         }
+        return fortunes;
     }
 
-    private static List<String> loadFortunes(String resourceName) {
-        String contents = readAll(resourceName);
-        String[] matches = divider.split(contents);
-        return Arrays.asList(matches).subList(1, matches.length);
+    private static List<String> loadFortunes(String ...resourceNames) {
+        List<String> fortunes = new ArrayList<>();
+        for (String resourceName : resourceNames) {
+            fortunes.addAll(loadFortunes(resourceName));
+        }
+        return fortunes;
     }
 
     public static void main(String[] args) {
-        List<String> fortunes = loadFortunes("/offensive.fortunes");
-        System.out.println(fortunes.get(5));
+        List<String> fortunes = loadFortunes("/offensive.fortunes", "/freebsd.fortunes");
+        Random random = new Random();
+        System.out.println(fortunes.get(random.nextInt(fortunes.size())));
     }
 }
