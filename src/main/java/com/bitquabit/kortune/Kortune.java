2107fd0be571534bac8da79575eb4663f1aed4e2
Benjamin Pollack <benjamin@bitquabit.com>
2018-02-18 14:12:47 -0500

    update Gradle

--- src/main/java/com/bitquabit/kortune/Kortune.java
+++ src/main/java/com/bitquabit/kortune/Kortune.java
@@ -4,10 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.regex.Pattern;
 
 public class Kortune {
     private static List<String> loadFortunes(String resourceName) {
